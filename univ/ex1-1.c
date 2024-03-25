20d6d6d75cc20d8576468f7bee2b0cfeee9a903f
SeongGwangJu <seonggwangdev@gmail.com>
2024-03-26 03:05:44 +0900

    first init
    add Hello.c

---
+++ univ/ex1-1.c
@@ -0,0 +1,9 @@
+/*	예제1-1 : Hello 출력하기
+	파일 이름 : ex1-1.c
+	수정 일자 : 2024년 3월 26일
+	작성자 : 주성광
+*/
+#include <stdio.h>          // 헤더 파일 추가
+void main() {              // main 함수 시작
+    printf("Hello~ \n");   // 모니터에 문자열 출력
+}                          // main함수 끝
